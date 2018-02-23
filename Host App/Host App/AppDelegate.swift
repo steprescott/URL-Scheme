db152846d222274f14136b5c84792263918d3ea4
Ste Prescott <ste.prescott@sky.uk>
2018-02-23 13:06:56 +0000

    Add empty host app

---
+++ Host App/Host App/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Host App
+//
+//  Created by Prescott, Ste on 23/02/2018.
+//  Copyright © 2018 Demo App. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
---
+++ Host App/Host App/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Host App
+//
+//  Created by Prescott, Ste on 23/02/2018.
+//  Copyright © 2018 Demo App. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
